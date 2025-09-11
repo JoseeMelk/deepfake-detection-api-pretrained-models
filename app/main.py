3b9463ac93191eb641f33657ec3554c258714ae5
JoseeMelk <moralescastellanos1234@gmail.com>
2025-09-10 23:10:13 -0600

    Versionado

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.routes.detect_deepfake_routes import router as deepfake_router
 app = FastAPI(
     title="Deepfake Detection API",
     description="API para detectar si una imagen es real o fake",
-    version="1.0.0"
+    version="2.0.0"
 )
 
 # Incluir rutas
